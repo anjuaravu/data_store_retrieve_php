b5d1b322a6f7669dd6441543cfd2fedee0121b1c
anjuaravu <98830774+anjuaravu@users.noreply.github.com>
2023-10-18 12:24:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,17 @@
 <?php
+
 $host="localhost";
 $username="root";
 $password="";
 $database="db2";
 
 $connection=mysqli_connect($host,$username,$password,$database);
+
 // if($connection){
 //     echo "connectn success";
 // }else{
 //     die("not succss");
 // }
-
-$name=$_GET['name'];
-$email=$_GET['email'];
-$mobile=$_GET['mobile'];
-$dob=$_GET['dob'];
-$gender=$_GET['gender'];
-
-$sql="INSERT INTO regform(name,email,mobile,dob,gender) VALUES('$name','$email','$mobile','$dob','$gender')";
-
-if(mysqli_query($connection,$sql)){
-    echo "record successfully";
-}else{
-    echo "not recorded ";
-}
-
 
 
 ?>
