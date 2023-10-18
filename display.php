ff8b1da9f755edd02bd2a3760d3c4c68826f0d71
anjuaravu <98830774+anjuaravu@users.noreply.github.com>
2023-10-18 12:24:00 +0530

    Update display.php

--- display.php
+++ display.php
@@ -1,45 +1,49 @@
-<?php
+<?php 
+
 include('index.php');
+
+
 $query="SELECT * from regform";
 $result=mysqli_query($connection,$query);
+
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>DISPLAY  DATA</h1>
-    <table>
+<body >
+    <h1>DISPLAY FORM DATA</h1>
+    <table border="1px">
     <tr>
-        <td>ID</td>
-        <td>NAME</td>
-        <td>EMAIL</td>
-        <td>MOBILE</td>
-        <td>DOB</td>
-        <td>Gender</td>
+        <th>ID</th>
+        <th>NAME</th>
+        <th>EMAIL</th>
+        <th>MOBILE</th>
+        <th>DOB</th>
+        <th>Gender</th>
     </tr>
-    <?php while($row=(mysqli_fetch_assoc($result)))
+
+    <?php 
+    
+    while($row=mysqli_fetch_assoc($result))
    {
    ?>
-   <tr>
-   <td> <?php echo $row=['id'] ?> </td>
-   <td> <?php echo $row=['$name'] ?> </td>
-    <td> <?php echo $row=['email'] ?> </td>
-    <td> <?php echo $row=['mobile'] ?> </td>
-    <td> <?php echo $row=['dob'] ?> </td>
-    <td> <?php echo $row=['gender'] ?> </td>
+
+    <tr>
+    <td> <?php echo $row['id']; ?> </td>
+    <td> <?php echo $row['name']; ?> </td>
+    <td> <?php echo $row['email']; ?> </td>
+    <td> <?php echo $row['mobile']; ?> </td>
+    <td> <?php echo $row['dob']; ?> </td>
+    <td> <?php echo $row['gender']; ?> </td>
     </tr>
     <?php
    }
-   mysqli_close($connection);
+
+  
     ?>
+
     </table>
 </body>
-</html> 
+</html>
 
 
 
